3403dedf966f4e3617e09af0b2d14628debedacf
s912a42 <s912a42@gmail.com>
2013-12-03 14:12:08 +0800

    all ver

--- main.cpp
+++ main.cpp
@@ -2,9 +2,7 @@
 
 using namespace std;
 
-double bmi  (double height,double kilo)                         //傳入身高與體重
-            {return ( kilo / ((height/100)*(height/100)) );     //換算為BMI值
-            };
+double bmi  (double height,double kilo);
 
 int main()
 {
@@ -16,3 +14,7 @@ int main()
     cout<<"你的bmi值為:"<<bmi(cm,kg)<<endl;                     //呼叫計算bmi的函數處理身高體重資料並輸出
     return 0;
 }
+
+double bmi  (double height,double kilo)                         //傳入身高與體重
+            {return ( kilo / ((height/100)*(height/100)) );     //換算為BMI值
+            };
